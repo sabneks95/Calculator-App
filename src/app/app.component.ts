b34297debc2d50a01ae8bfeac9cea774dd784030
sabneks95 <95006362+sabneks95@users.noreply.github.com>
2022-11-29 20:17:22 +0530

    Calculator App first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'CalculatorApp';
+  input1: number = 0;
+  input2: number = 0;
+  result: number = 0;
+
+  add() {
+    this.result = this.input1 + this.input2;
+  }
+
+  sub() {
+    this.result = this.input1 - this.input2;
+  }
+
+  mul() {
+    this.result = this.input1 * this.input2;
+  }
+
+  divide() {
+    this.result = this.input1 / this.input2;
+  }
 }
